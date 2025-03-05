3add66416e469b0acfa4b24a8571429fe3897c4e
akalpit23 <akalpitdawkhar10@gmail.com>
2025-03-05 00:19:05 -0500

    Working App

--- Utils/Config.swift
+++ Utils/Config.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Akalpit Dawkhar on 3/3/25.
 //
-// Utils/Config.swift
+
 import Foundation
 
 enum Config {
@@ -12,7 +12,7 @@ enum Config {
         // Read values from Info.plist, which gets them from config.xcconfig
         static let url = Bundle.main.infoDictionary?["SUPABASE_URL"] as? String ?? ""
         static let anonKey = Bundle.main.infoDictionary?["SUPABASE_ANON_KEY"] as? String ?? ""
-        static let developmentURL = "https://your-development-url.supabase.co" // Fallback for development
-        static let developmentAnonKey = "your-development-key" // Fallback for development
+        static let developmentURL = "https://eegopypfewyqioxirefy.supabase.co" // Fallback for development
+        static let developmentAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZ29weXBmZXd5cWlveGlyZWZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTUyODEsImV4cCI6MjA1NTU3MTI4MX0.C38C1W6Wsjk-p92xFcOyNA5fRclbKFjM5WQsc6ZCZVw" // Fallback for development
     }
 }
